4b2107387f2516800029ca04643d694c68740b32
davitshirkhanyan <davit.shirkhanyan@gmail.com>
2021-01-17 11:01:24 -0800

    add bootstrap classes and time interval

---
+++ Develop/js/script.js
@@ -0,0 +1,7 @@
+$("#currentDay").text(moment().format("MMMM DD YYYY hh:mm:ss A"));
+
+let currentDate = function () {
+$("#currentDay").text(moment().format("MMMM DD YYYY hh:mm:ss A"));
+};
+
+setInterval(currentDate, 1000);
--- assets/js/script.js
+++
@@ -1,1 +0,0 @@
-$('#currentDay').moment().toNow();
