0bcbe8ca39584d859e35d455128161fa828b6978
davitshirkhanyan <davit.shirkhanyan@gmail.com>
2021-01-17 20:23:15 -0800

    update

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,7 +1,27 @@
+
 $("#currentDay").text(moment().format("MMMM DD YYYY hh:mm:ss A"));
 
 let currentDate = function () {
 $("#currentDay").text(moment().format("MMMM DD YYYY hh:mm:ss A"));
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 setInterval(currentDate, 1000);
