cba11c69ac2b04eb3694d73c6a7a13c599027e3d
davitshirkhanyan <davit.shirkhanyan@gmail.com>
2021-01-20 14:59:48 +0700

    update more codes

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -4,69 +4,43 @@ let currentDate = function () {
 $("#currentDay").text(moment().format("MMMM DD YYYY hh:mm:ss A"));
 };
 
-// add work schedule variable 
-let workHours = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17"];
-
+// add main function
 $(document).ready(function() {
-    if(!localStorage.getItem('workSchedule')) {
-        updateWorkSchedule(workHours);
-    } else {
-        updateWorkSchedule(JSON.parse(localStorage.getItem('workSchedule')));
-        updatetime();
-    }
+setInterval(currentDate, 1000);
+updateWorkScheduleColor();
+getWorkScheduleEvent();
 });
-
+   
 // add function to update the date with colors
-function updatetime() {
-  let currentTime = moment().format('H');
+function updateWorkScheduleColor(){
+    $("textarea").each(function(){
+        let currentHour = moment().hour();
+        let workScheduleHour = parseInt($(this).attr("id"));
 
-  for(let i = 0; i < workHours.length; i++) {
-    if (parseInt(workHours[i]) > currentTime) {
-      $("#text-" + i).removeClass("past");
-      $("#text-" + i).addClass("future");
-    }
-    else if (parseInt(workHours[i]) < currentTime) {
-      $("#text-" + i).addClass("past");
-    }
-    else {
-      $("#text-" + i).removeClass("past");
-      $("#text-" + i).addClass("present");
-    }
-  }
-};
-
-// add functions to save items in localstorage
-let saveLocalStorage = function() {
-    localStorage.setItem('workSchedule', JSON.stringify());
-};
-
-let saveObjToLocalStorage = function(obj) {
-    localStorage.setItem('workSchedule', JSON.stringify(obj));
-};
-
-// add functions to get items from localstorage
-let getScheduleDay = function(key, value) {
-    if(!localStorage.getItem('workSchedule')) {
-        saveLocalStorage();
-    } else {
-        let workDay = JSON.parse(localStorage.getItem('workSchedule'));
-        workDay[key] = value;
-        saveObjToLocalStorage(workDay);
-    }
-};
-
-let updateWorkSchedule = function(dayObj) {
-    $(".row").each(function() {
-        let result = $(this).children("div");
-        $(this).children("textarea").text(dayObj[result.text()]);
+        if (workScheduleHour === currentHour){
+            $(this).addClass("present");
+        } else if (workScheduleHour < currentHour){
+            $(this).addClass("past");
+        } else {
+            $(this).addClass("future");
+        };
     });
 };
 
-// use save button function
+// use save button function to set the item in local storage
 $("button").on("click", function(key, value) {
-    key = $(this).siblings("div").text();
-    value = $(this).siblings("textarea").val();
-    getScheduleDay(key, value);
+    key = $(this).siblings(".work-event").attr("id");
+    value = $(this).siblings(".work-event").val();
+    localStorage.setItem(key, value)
 });
 
-setInterval(currentDate, 1000);
+// add function to get the item from local storage
+let getWorkScheduleEvent = function() {
+    $(".work-event").each(function() {
+        let result = $(this).attr("id");
+        $(this).val(localStorage.getItem(result));
+    });
+};
+
+
+
